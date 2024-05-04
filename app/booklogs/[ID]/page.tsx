f4c683ef3f1713c39f83abdc60ec117c0d21c356
frontflg <50544382+frontflg@users.noreply.github.com>
2024-05-04 15:37:07 +0900

    Add files via upload

---
+++ app/booklogs/[ID]/page.tsx
@@ -0,0 +1,174 @@
+import prisma from '@/lib/prisma';
+import Link from 'next/link';
+import Image from 'next/image';
+import { revalidatePath } from 'next/cache';
+// import { useRouter } from 'next/navigation';
+
+export default async function Page({ params }: { params: { ID: string } }) {
+  const id = String(params.ID);
+  const booklog = await prisma.booklog.findUnique({
+    where: {
+      isbn13: id,
+    },
+  })
+  return (
+    <div className="m-8">
+      <Link href="/booklogs" className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white whitespace-nowrap">戻る</Link>
+      <h1 className="text-xl font-bold">BOOKLOG詳細</h1>
+      <form action={async (data: FormData) => {
+          'use server';
+          const isbn13      = data.get('isbn13') as string;
+          const isbn10      = data.get('isbn10') as string;
+          const bookname    = data.get('bookname') as string;
+          const author      = data.get('author') as string;
+          const publisher   = data.get('publisher') as string;
+          const genre       = data.get('genre') as string;
+          const issuedate   = data.get('issuedate') as date;
+          const getdate     = data.get('getdate') as date;
+          const readdate    = data.get('readdate') as date;
+          const ownership   = parseInt(data.get('ownership'));
+          const purchase    = parseInt(data.get('purchase'));
+          const library     = data.get('library') as string;
+          const overview    = data.get('overview') as string;
+          const impressions = data.get('impressions') as string;
+          const state       = data.get('state') as string;
+          await prisma.booklog.update({
+            where: {
+              isbn13: id,
+            },
+            data: {
+              isbn10,
+              bookname,
+              author,
+              publisher,
+              genre,
+              issuedate: new Date(issuedate.replaceAll('/', '-')),
+              getdate:   new Date(getdate.replaceAll('/', '-')),
+              readdate:  new Date(readdate.replaceAll('/', '-')),
+              ownership,
+              purchase,
+              library,
+              overview,
+              impressions,
+              state,
+            },
+          });
+       // const router = useRouter();
+       // router.back();
+        }}
+      >
+      <table className="table-auto">
+      <tbody>
+        <tr className="px-4 py-2 border">
+          <th>ISBN13:</th>
+          <td width={500} className="px-2 bg-gray-200" colspan="5">{booklog?.isbn13}
+          <input type="hidden" name="isbn13" value={booklog?.isbn13} />
+          </td>
+          <th className="bg-gray-200">表　紙:</th></tr>
+        <tr className="px-4 py-2 border">
+          <th className="px-1 bg-gray-200">ISBN10:</th>
+          <td className="px-2" colspan="5">
+            <input type="number"
+                   name="isbn10"
+                   defaultValue={`${booklog?.isbn10}`}
+                   size="7"
+                   min="1000000000"
+                   max="9999999999"
+            />
+          </td>
+          <td rowspan="13">
+            <Image
+              height={500}
+              width={300}
+              src={`https://images-fe.ssl-images-amazon.com/images/P/${booklog?.isbn10}.09.LZZZZZZZ`}
+            /></td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">書　名:</th>
+          <td className="px-2" colspan="5">
+            <input type="text" name="bookname" defaultValue={`${booklog?.bookname}`} size="50" />
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">著　者:</th>
+          <td className="px-2" colspan="5">
+            <input type="text" name="author" defaultValue={`${booklog?.author}`} size="25" />
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">出版社:</th>
+          <td className="px-2" colspan="5">
+            <input type="text" name="publisher" defaultValue={`${booklog?.publisher}`} size="25" />
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">分　野:</th>
+          <td className="px-2" colspan="5">
+            <input type="text" name="genre" defaultValue={`${booklog?.genre}`} size="25" />
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">発行日:</th>
+          <td className="px-2">
+            <input type="text" name="issuedate" size="10"
+             defaultValue={`${booklog?.issuedate.toLocaleDateString().replaceAll('/', '-')}`} /></td>
+          <th className="bg-gray-200">取得日:</th>
+          <td className="px-2">
+            <input type="text" name="getdate" size="10"
+             defaultValue={`${booklog?.getdate.toLocaleDateString().replaceAll('/', '-')}`} /></td>
+          <th className="bg-gray-200">読了日:</th>
+          <td className="px-2">
+            <input type="text" name="readdate" size="10"
+             defaultValue={`${booklog?.readdate.toLocaleDateString().replaceAll('/', '-')}`} /></td>
+          </tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">所　有:</th>
+          <td className="px-2" colspan="5">
+            <input type="number" name="ownership" defaultValue={`${booklog?.ownership}`} />
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">価　格:</th>
+          <td className="px-2" colspan="5">
+            <input type="number" name="purchase" defaultValue={`${booklog?.purchase}`} />
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">図書館:</th>
+          <td className="px-2" colspan="5">
+            <input type="text" name="library" defaultValue={`${booklog?.library}`} size="25" />
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">概　要:</th>
+          <td className="px-2" colspan="5">
+            <textarea name="overview" cols="62">{booklog?.overview}</textarea>
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">感　想:</th>
+          <td className="px-2" colspan="5">
+            <textarea name="impressions" cols="62">{booklog?.impressions}</textarea>
+          </td></tr>
+        <tr className="px-4 py-2 border">
+          <th className="bg-gray-200">状　態:</th>
+          <td className="px-2" colspan="5">
+            <input type="text" name="state" defaultValue={`${booklog?.state}`} size="10" />
+          </td></tr>
+      </tbody>
+      </table>
+      <button type="submit"
+        className="bg-green-500 my-4 px-2 py-1 rounded-lg text-sm text-white">
+        更新
+      </button>
+      </form>
+
+      <form action={async (data: FormData) => {
+          'use server';
+          const isbn13 = data.get('isbn13') as string;
+          await prisma.booklog.delete({ where: { isbn13 } });
+       // const router = useRouter();
+       // router.back();
+        }}
+      >
+        <input type="hidden" name="isbn13" value={booklog?.isbn13} />
+        <button type="submit"
+          className="bg-red-500 px-2 py-1 rounded-lg text-sm text-white whitespace-nowrap">
+          削除
+        </button>
+      </form>
+
+    </div>
+  );
+}
---
+++ app/booklogs/page.tsx
@@ -0,0 +1,61 @@
+import prisma from '@/lib/prisma';
+import Link from 'next/link';
+import { revalidatePath } from 'next/cache';
+
+const Page = async () => {
+  const booklog = await prisma.booklog.findMany();
+
+  return (
+    <div className="m-8">
+      <h1 className="text-xl font-bold">BOOKLOG一覧</h1>
+      <div class="h-96 overflow-scroll">
+        <table className="table-auto">
+          <thead className="sticky top-0 z-10">
+            <tr className="bg-gray-200">
+              <th className="sticky left-0  bg-white border">ISBN13</th>
+              <th className="px-4 py-2 border">書名</th>
+              <th className="px-4 py-2 border">著者名</th>
+              <th className="px-4 py-2 border">発行</th>
+              <th className="px-4 py-2 border">分類</th>
+            </tr>
+          </thead>
+          <tbody>
+            {booklog.map((booklog) => (
+              <tr key={booklog.isbn13}>
+                <td className=" px-4 py-2 sticky left-0 z-[2] bg-slate-100 border ">
+                  <Link href={`/booklogs/${booklog.isbn13}`}>{booklog.isbn13}</Link>
+                </td>
+                <td className=" px-4 py-2 border">{booklog.bookname}</td>
+                <td className=" px-4 py-2 border">{booklog.author}</td>
+                <td className=" px-4 py-2 border">{booklog.publisher}</td>
+                <td className=" px-4 py-2 border">{booklog.genre}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <form className="flex items-center mt-4"
+        action={async (data: FormData) => {
+          'use server';
+          const isbn13 = data.get('isbn13') as string;
+          const bookname = data.get('bookname') as string;
+          await prisma.booklog.create({ data: { isbn13, bookname } });
+          revalidatePath('/booklogs');
+        }}
+      >
+        <label htmlFor="isbn13">ISBN13:</label>
+        <input type="text" name="isbn13" className="border mx-2 p-1" />
+        <label htmlFor="bookname">書名:</label>
+        <input type="text" name="bookname" className="border mx-2 p-1" />
+        <button
+          type="submit"
+          className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white"
+        >
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Page;
